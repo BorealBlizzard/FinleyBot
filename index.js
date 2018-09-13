d0c910971d8efbdec2cf6306ba508e3c83376bdc
Jacob Rahman <rapter476@outlook.com>
2018-09-13 16:48:03 -0500

    added ping/pong

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready",async () => {
   console.log(`${bot.user.username} is online!`);
 });
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
 
 bot.login(botconfig.token);
 }
