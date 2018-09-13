82589e9096b8f36a0241bd27509cd53c867840a3
Jacob Rahman <rapter476@outlook.com>
2018-09-13 18:31:10 -0500

    Added some commands
    added
    f!ping
    f!hello
    f!panzer vor

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 {
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "f!"
 
-const bot = new Discord.Client({disableEveryone: true});
-
-bot.on("ready",async () => {
-  console.log(`${bot.user.username} is online!`);
+client.on("ready", () => {
+  console.log("I am ready!");
 });
 
-bot.login(botconfig.token);
+client.on("message", (message) => {
+  if (message.content.startsWith(prefix + "ping")) {
+    message.reply("pong!");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith(prefix + "hello")) {
+    message.channel.send("owo meow");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith(prefix + "panzer vor")) {
+    message.channel.send("Jawohl, Start Panzerkampfwagen VI Tiger Ausf. E ", {files: ["./images/TigerI.png"]} );
+ }
+});
+
+client.login(botconfig.token);
 }
