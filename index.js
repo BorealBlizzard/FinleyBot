c26e4b681a7f7d08689eff0ba1b433a378b5a1f7
Jacob Rahman <rapter476@outlook.com>
2018-09-13 16:55:56 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready",async () => {
   console.log(`${bot.user.username} is online!`);
 });
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
-});
 
 bot.login(botconfig.token);
 }
