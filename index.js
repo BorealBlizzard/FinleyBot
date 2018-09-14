ed5f046e8fcfad286656937ebed7dee23f2f3c52
Jacob <rapter476@outlook.com>
2018-09-14 10:30:24 -0500

    Added TigerI edit and cleaned up some code

--- index.js
+++ index.js
@@ -2,27 +2,31 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "f!"
+const prefix = (botconfig.prefix)
 
+//Confirms the bot is online
 client.on("ready", () => {
   console.log("Meowzers, I'm ready to go!");
 });
 
+//basic Ping/Pong command
 client.on("message", (message) => {
   if (message.content.startsWith(prefix + "ping")) {
     message.reply("pong!");
   }
 });
 
+//basic reply command with user mention
 client.on("message", (message) => {
   if (message.content.startsWith(prefix + "hello")) {
-    message.channel.send("owo, hello" + message.author.username);
+    message.channel.send("owo, hello ${sent.author.username}");
   }
 });
 
+//panzer vor meme
 client.on("message", (message) => {
   if (message.content.startsWith(prefix + "panzer vor")) {
-    message.channel.send("Jawohl, Start Panzerkampfwagen VI Tiger Ausf. E ", {files: ["./images/TigerI.png"] });
+    message.channel.send("Jawohl, Start Panzerkampfwagen VI Tiger Ausf. E ", {files: ["./images/TigerIKat.png"] });
  }
 });
 
