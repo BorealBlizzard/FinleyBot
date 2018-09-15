e34523528684059a495523b389b6c81c49f5e5a6
Jacob Rahman <rapter476@outlook.com>
2018-09-14 21:16:42 -0500

    Changed things, I cant remember what. did some clean up

--- index.js
+++ index.js
@@ -2,41 +2,46 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = (botconfig.prefix)
+const prefix = (botconfig.prefix);
+
+//creating "command" variuble to simplify commands
+//const args = message.content.slice(prefix.length).trim().split(/ +/g);
+//const command = args.shift().toLowerCase();
 
 
 //Confirms the bot is online
 client.on("ready", async () => {
-  console.log(`Meowzers, ${client.user.username} is ready to go!`);
+  console.log(`Meowzers, ${client.user.username} is ready to go! Connected to ${client.guilds.size} servers, for ${client.users.size} users.`);
 
   client.user.setActivity("with a ball of yarn",{type: "PLAYING"})
 });
 
-//basic Ping/Pong command
+
 client.on("message", (message) => {
+if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+//basic Ping/Pong command
   if (message.content.startsWith(`${prefix}ping`)) {
     message.reply("pong!");
-    }
-});
+
+} else
 
 //basic reply command with user mention
-client.on("message", (message) => {
   if (message.content.startsWith(`${prefix}hello`)) {
     message.channel.send(`owo, hello ${message.author}`);
-  }
-});
+
+  } else
 
 //panzer vor meme
-client.on("message", (message) => {
-  if (message.content.startsWith(prefix + "panzer vor")) {
+  if (message.content.startsWith(prefix + "panzer vor") ){
     message.channel.send("Jawohl, Start Panzerkampfwagen VI Tiger Ausf. E ", {files: ["./images/TigerIKat.png"] });
- }
-});
+
+  } else
 
 //DM command
-client.on("message", (message) => {
   if (message.content.startsWith(prefix + "private")) {
     message.author.send("meow");
+
   }
 })
 
